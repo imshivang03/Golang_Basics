5e86b5c852bc3318c11214310803010abf1e77c0
Shivang Shritu <49519425+imshivang03@users.noreply.github.com>
2020-10-16 23:23:15 +0530

    Create relational_operator.go

---
+++ relational_operator.go
@@ -0,0 +1,28 @@
+package main 
+   
+import "fmt"
+   
+func main() { 
+   p:= 34
+   q:= 20
+       
+   result1:= p == q 
+   fmt.Println(result1) 
+      
+   result2:= p != q 
+   fmt.Println(result2) 
+      
+   result3:= p < q 
+   fmt.Println(result3) 
+      
+   result4:= p > q 
+   fmt.Println(result4) 
+      
+   result5:= p >= q 
+   fmt.Println(result5) 
+       
+   result6:= p <= q 
+   fmt.Println(result6) 
+      
+      
+} 
