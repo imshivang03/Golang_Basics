d3a23b55e0eedfa56a8ff70215a4bfacc03d1c58
Shivang Shritu <49519425+imshivang03@users.noreply.github.com>
2020-10-16 23:55:28 +0530

    Create Loop.go

---
+++ Loop.go
@@ -0,0 +1,21 @@
+package main 
+  
+import "fmt"
+  
+func main() { 
+      
+   
+    chnl := make(chan int) 
+    go func(){ 
+        chnl <- 100 
+        chnl <- 1000 
+       chnl <- 10000 
+       chnl <- 100000 
+       close(chnl) 
+    }() 
+
+    for i:= range chnl { 
+       fmt.Println(i)  
+    } 
+    
+} 
